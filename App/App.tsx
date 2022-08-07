1dccce1fc34c96ff174da8b892f2a48356cf193e
Ted Pattison <ted@tedpattison.net>
2022-08-07 18:57:01 -0400

    Updates

--- App/App.tsx
+++ App/App.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import { BrowserRouter } from "react-router-dom";
-
-import { useIsAuthenticated } from "@azure/msal-react";
-
 import PageLayout from './components/pagelayout'
 import { Box, CssBaseline } from '@mui/material';
 
 const App = () => {
-  const isAuthenticated = useIsAuthenticated();
-
   return (
     <Box>
       <CssBaseline />
